86d444148182fc900837298131322633df92caa2
Mehul Rao <mehulrao@gmail.com>
2022-12-23 13:18:20 -0500

    update seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,43 +11,21 @@ import "@fontsource/sen/700.css";
 import { NextSeo } from "next-seo";
 import Head from "next/head";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
-            <Script
-                strategy="lazyOnload"
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-            />
-
-            <Script id="google-analytics" strategy="lazyOnload">
-                {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-                `}
-            </Script>
             <NextSeo
                 title="Mehul | Software Engineer Intern"
                 titleTemplate="Mehul | Software Engineer Intern"
                 defaultTitle="Mehul | Software Engineer Intern"
                 description="Hey! I'm Mehul, A Software Enginner and a Student!"
                 openGraph={{
-                    url: "https://mehulrao.com",
+                    url: "https://mehulrao.dev",
                     title: "Mehul | Software Engineer Intern",
                     description:
                         "Hey! I'm Mehul, A Software Enginner and a Student!",
-                    images: [
-                        {
-                            url: "https://res.cloudinary.com/ddum5vpp3/image/upload/v1643532760/og-image_dwcwhp.png",
-                            width: 800,
-                            height: 420,
-                            alt: "Mehul | Software Engineer Intern",
-                        },
-                    ],
                 }}
                 twitter={{
                     handle: "@mehulrao13",
@@ -58,7 +36,7 @@ function MyApp({ Component, pageProps }: AppProps) {
                     {
                         property: "keywords",
                         content:
-                            "Frontend Developer, anurag, anuragkr, Web Developer, web development, web developer, blogger, tech enthusiast, open source",
+                            "Frontend Developer, mehulrao, Mehul Rao, Web Developer, web development, web developer, tech enthusiast, open source",
                     },
                 ]}
             />
