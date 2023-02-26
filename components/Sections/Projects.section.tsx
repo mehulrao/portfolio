0d132ce1b425c972fa1d7796deaefbb57fb3dda7
Mehul Rao <mehulrao@gmail.com>
2023-02-26 15:55:57 -0500

    add treehacks project

--- components/Sections/Projects.section.tsx
+++ components/Sections/Projects.section.tsx
@@ -7,47 +7,56 @@ const Projects: NextComponentType = () => {
         <div className="my-16 px-3 font-sen" id="projects">
             <p className="text-3xl font-bold text-white">Featured Projects</p>
             <div className="my-8 flex flex-col items-center justify-center gap-10 sm:flex-row">
-                <Link href="https://github.com/mehulrao/Chess-AI" passHref>
+                <Link href="https://github.com/mehulrao/Racecar-ML" passHref>
                     <a
-                        className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#1d1686] to-[#5E72EB] p-1 text-white duration-100 hover:scale-105"
+                        className="h-[9rem] w-[16rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#1d1686] to-[#5E72EB] p-1 text-white duration-100 hover:scale-105"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary from-[#1d1686] to-[#5E72EB] px-2 text-center font-medium hover:bg-gradient-to-r hover:text-primary">
-                            <p className="text-xl font-semibold">Chess AI</p>
-                            <p>
-                                Competitive chess AI <br />
-                                Written in Java
+                        <div className="flex h-full w-full flex-col rounded-lg bg-primary from-[#1d1686] to-[#5E72EB] px-2 text-center font-medium hover:bg-gradient-to-r hover:text-primary">
+                            <p className="pt-3 text-xl font-semibold">
+                                Racecar ML
+                            </p>
+                            <div className="pb-3" />
+                            <p className="items-center justify-center">
+                                Self Driving MIT RACECAR <br />
+                                Written in C# and Built Using Unity
                             </p>
                         </div>
                     </a>
                 </Link>
 
-                <Link href="https://github.com/mehulrao/Racecar-ML" passHref>
+                <Link href="https://github.com/mehulrao/Lav-U" passHref>
                     <a
-                        className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#5E72EB] to-[#FF9190] p-1 text-white duration-100 hover:scale-105"
+                        className="h-[9rem] w-[16rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#5E72EB] to-[#FF9190] p-1 text-white duration-100 hover:scale-105"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
                         <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary from-[#5E72EB] to-[#FF9190] px-2 text-center font-medium hover:bg-gradient-to-r hover:text-primary">
-                            <p className="text-xl font-semibold">Racecar ML</p>
-                            <p>
-                                Self Driving MIT RACECAR <br />
-                                Built Using C# and Unity
+                            <p className="pt-3 text-xl font-semibold">LavÜ</p>
+                            <div className="pb-3" />
+                            <p className="items-center justify-center text-sm">
+                                Back Mounted Stress Management Device <br />
+                                Built at TreeHacks Using C++ and React Native
                             </p>
                         </div>
                     </a>
                 </Link>
 
-                <Link href="https://github.com/mehulrao/2D-Game" passHref>
+                <Link href="https://github.com/mehulrao/Chess-AI" passHref>
                     <a
-                        className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#FF9190] to-[#FDC094] p-1 text-white duration-100 hover:scale-105"
+                        className="h-[9rem] w-[16rem] cursor-pointer rounded-lg bg-gradient-to-r from-[#FF9190] to-[#FDC094] p-1 text-white duration-100 hover:scale-105"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary from-[#FF9190] to-[#FDC094] px-2 text-center font-medium hover:bg-gradient-to-r hover:text-primary">
-                            <p className="text-xl font-semibold">
-                                2D Platformer Game
+                        <div className="flex h-full w-full flex-col rounded-lg bg-primary from-[#FF9190] to-[#FDC094] px-2 text-center font-medium hover:bg-gradient-to-r hover:text-primary">
+                            <p className="pt-3 text-xl font-semibold">
+                                Chess AI
+                            </p>
+                            <div className="pb-3" />
+                            <p className="items-center justify-center">
+                                Competitive Chess AI <br />
+                                Written in Java
                             </p>
                         </div>
                     </a>
