c08fcd26a29edbd51902c886000a16ab2e5b0588
Mehul Rao <mehulrao@gmail.com>
2023-09-20 14:29:06 -0400

    change skills

--- components/Sections/Skills.section.tsx
+++ components/Sections/Skills.section.tsx
@@ -9,7 +9,7 @@ const Skills = () => {
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300 ring-zinc-300 duration-150 hover:scale-105 hover:ring-2">
           <BsArrowRightShort size="30" />
           <span className="text-white">Java</span>
-          &nbsp;as my main language
+          &nbsp;as my Main Language
         </p>
 
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300 ring-zinc-300 duration-150 hover:scale-105 hover:ring-2">
@@ -20,30 +20,32 @@ const Skills = () => {
 
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300 ring-zinc-300 duration-150 hover:scale-105 hover:ring-2">
           <BsArrowRightShort size="30" />
-          <span className="text-white">Tailwind CSS </span>
-          &nbsp;as my CSS framework
+          <span className="text-white">NextJS </span>
+          &nbsp;as my Javascript Framework
         </p>
 
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300 ring-zinc-300 duration-150 hover:scale-105 hover:ring-2">
           <BsArrowRightShort size="30" />
           <span className="text-white">Unity </span>
-          &nbsp;as my Game Development framework
+          &nbsp;as my Game Development Framework
         </p>
 
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300 ring-zinc-300 duration-150 hover:scale-105 hover:ring-2">
           <BsArrowRightShort size="30" />
           <span className="text-white">Terraform </span>
-          &nbsp;as my DevOPS framework
+          &nbsp;as my DevOPS Framework
         </p>
       </div>
 
       <p className="text-lg font-medium text-slate-300">
-        ...more skills include{" "}
-        <span className="text-yellow-600">javascript</span>,{" "}
+        ...more skills include <span className="text-yellow-400">python</span>,{" "}
         <span className="text-blue-400">reactjs</span>,{" "}
-        <span className="text-purple-400">C#</span>,{" "}
+        <span className="text-green-400">css</span>,{" "}
+        <span className="text-purple-400">php</span>,{" "}
+        <span className="text-orange-400">docker</span>,{" "}
+        <span className="text-red-400">ansible</span>,{" "}
         <span className="text-white">and</span>{" "}
-        <span className="text-yellow-400">python</span>,{" "}
+        <span className="text-indigo-400">wordpress</span>,{" "}
       </p>
     </div>
   );
